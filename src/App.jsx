70696c3e4d6fccd271ac07673b80f0746601bc05
Subashkannan <subashkannan0309@gmail.com>
2025-12-23 17:16:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Task1 from "./Components/Task1";
+import Task2 from "./Components/Task2";
+
+const App = () =>{
+
+
+  return (    <div>
+      {/* <Task1 /> */}
+      <Task2 />
+    </div>
+  )
+}
+export default App;
---
+++ src/Components/Task1.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Task1= () => {
+  const ProductCard = ({ name, price }) => {
+    return (
+      <div>
+        <h3>{name}</h3>
+        <p>₹{price}</p>
+      </div>
+    );
+  };
+
+ 
+  const products = [
+    { id: 1, name: "Laptop", price: 55000 },
+    { id: 2, name: "Mobile", price: 25000},
+    { id: 3, name: "Headphones", price: 3000 },
+    { id: 4, name: "Keyboard", price: 1500 },
+    { id: 5, name: "Mouse", price: 800 }
+  ];
+
+  return (
+    <div>
+      <h2>Product List</h2>
+
+      <div>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}     
+            name={product.name}
+            price={product.price}
+            />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+
+export default Task1;
